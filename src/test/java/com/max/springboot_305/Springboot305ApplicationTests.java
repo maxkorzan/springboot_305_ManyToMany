f07d1dd5c54a3f5fb65d3b1d8677b8f3ea63b10f
GBTC440004UR <GBTC440004UR@MC.MONTGOMERYCOLLEGE.EDU>
2020-02-25 10:58:32 -0500

    first commit

---
+++ src/test/java/com/max/springboot_305/Springboot305ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.max.springboot_305;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot305ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
